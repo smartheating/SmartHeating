a562f064b9359c799d8e34b54c7bc535ee0d7a8e
Pascal Weiß <pa-weiss@gmx.de>
2019-06-06 17:01:17 +0200

    Added and tested scripts for all relevant aws operations

---
+++ aws/bin/lib_smh/smart_heating_aws.py
@@ -0,0 +1,19 @@
+import boto3
+from tabulate import tabulate
+
+
+def get_instances(state_name='running') -> boto3:
+    if isinstance(state_name, list): states = state_name
+    else: states = [state_name]
+    ec2 = boto3.resource('ec2')
+    filters = [{'Name': 'instance-state-name', 'Values': states}]
+    instances = [_ for _ in ec2.instances.filter(Filters=filters)]
+    return instances
+
+
+def print_table(instances):
+    tab_data = [['[{}]'.format(i), instance.tags[0]['Value'], instance.id, instance.public_ip_address, instance.state['Name']]
+                for instance, i in zip(instances, range(1, len(instances) + 1))]
+    print(tabulate(tab_data, headers=['[number]', 'name', 'id', 'ip', 'state']))
+
+
